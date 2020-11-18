fdb86c1e58e60d00fed4291a502553f3525afc20
utrape <pablosud025@gmail.com>
2020-11-18 16:49:17 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import PySimpleGUI as sg
+import webscrappy
+sg.Input()
+sg.theme('Dark')
+layout = [
+    [sg.T('Search'), sg.In(default_text='', key='input', size=(30,10)), sg.B(button_text='search', size=(6, 1), key='search'), sg.B(button_text='hear', key='hear')],
+    
+    [sg.T(key='answer', size=(50, 5), background_color = 'grey', text_color='Black', pad=(50, 13))],
+    [sg.B('help', key='help')]
+]
+
+window = sg.Window('Dictionary', layout)
+
+while True:
+    
+    event, values = window.read()
+    
+    if event == sg.WIN_CLOSED:
+        break
+
+    if event == 'search':
+        if len(values['input']) == 0:
+            window['answer'].update('write something')
+        else:
+            webscrappy.search(values['input'])
+            window['answer'].update(webscrappy.returnMeaning())
+    
+    if event == 'hear':
+        webscrappy.playonthesound()
+        
+    if event == 'help':
+        sg.popup('This is a program that searches for any word you choose and shows the first meaning found for that word according to the dictionary "https://dictionary.cambridge.org/pt/"', title='help')
+
+
+webscrappy.localquit()
---
+++ webscrappy.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+
+
+options = Options()
+options.headless = True 
+driver = webdriver.Chrome(executable_path='chrome/chromedriver', options=options)
+ 
+    
+def search(word):
+    driver.get(f'https://dictionary.cambridge.org/pt/dicionario/ingles/{word}')
+
+
+    
+def returnMeaning():
+    try:
+        return driver.find_element_by_class_name('db').text
+    except:
+        return 'Not found'
+
+def playonthesound():
+    try:
+        hear = driver.find_element_by_class_name('i-volume-up')
+        hear.click()
+    except:
+        ...
+    
+    
+def localquit():
+    driver.quit()
