f80310dd2d0dc677bd06c05294a32feffbceb519
utrape <pablosud025@gmail.com>
2020-11-21 12:10:58 -0300

    add Spanish option and changing the way that instance works

--- main.py
+++ main.py
@@ -1,65 +1,55 @@
 import PySimpleGUI as sg
-from webscrapy import English, Portuguese
+from webscrapy import English, Portuguese, Spanish
 
 sg.theme('Dark')
 
 layout = [
-    [sg.T('Search'), sg.In(default_text='', key='input', size=(30,10)), sg.B    (button_text='search', size=(6, 1), key='search'), sg.B(button_text='hear', key='hear'), sg.DropDown(['English', 'Portuguese'], default_value='English', key='options', enable_events=True)],
+    [sg.T('Search'), sg.In(default_text='', key='input', size=(30,10)), sg.B    (button_text='search', size=(6, 1), key='search'), sg.B(button_text='hear', key='hear'), sg.DropDown(['English', 'Portuguese', 'Spanish'], default_value='English', key='options', enable_events=True)],
     [sg.Multiline(key='answer', size=(65, 10), text_color='Black', background_color='grey', disabled=True)],
     [sg.B('help', key='help')]]
 
 window = sg.Window('MultiLanguage Dictionary', layout)
 
-english = English()
-language = list()
+dictionary = English(False)
+
 while True:
     event, values = window.read()
     
-    try:
-        language.append(values['options'])
-    except:
-        ...
-        
     if event == sg.WIN_CLOSED:
         break
 
     if event == 'options':
         new_language = values['options']
-        if language != new_language:
+        if dictionary.__repr__() != new_language:
             if new_language == 'Portuguese':
-                del english
-                portuguese = Portuguese()
+                del dictionary
+                dictionary = Portuguese(False)
+            elif new_language == 'English':
+                del dictionary
+                dictionary = English(False)
+            
             else:
-                del portuguese
-                english = English()
+                del dictionary
+                dictionary = Spanish(False)
+                
         else:
             ...
-            
-        while True:
-            if len(language) == 1:
-                break
-            language.pop(0)
-                        
+                                                      
     if event == 'search':
         if len(values['input']) == 0:
             window['answer'].update('write something')
         else:
-            if language[0] == 'English':
-                window['answer'].update(english.returnMeaning(values['input']))
-            elif language[0] == 'Portuguese':
-                window['answer'].update(portuguese.returnMeaning(values['input']))
+            window['answer'].update(dictionary.returnMeaning(values['input']))
+
     
     if event == 'hear':
-        if language[0] == 'English':
-            english.playonthesound()
-        elif language[0] == 'Portuguese':
-            sg.popup('The sound is not working in Portuguese', title='warning')
+        if dictionary.__repr__() == 'English':
+            dictionary.playonthesound()
+        elif dictionary.__repr__() == 'Portuguese' or dictionary.__repr__() == 'Spanish':
+            sg.popup('The sound is not working in Portuguese/Spanish', title='warning')
                  
     if event == 'help':
         sg.popup('This is the Multi language program that take your language choice and one word this language and show your definition, able play the word\'s sound\nlanguage options: English and Portuguese', title='help')
 
 
-try:
-    del portuguese
-except:
-    del english
+del dictionary
