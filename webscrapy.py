bdd97dfdf2cb5a1efb5407f025d62b390b7296ea
utrape <pablosud025@gmail.com>
2021-02-03 14:14:42 -0300

    webscrapy restructured

--- webscrapy.py
+++ webscrapy.py
@@ -1,6 +1,7 @@
 import requests
 from parsel import Selector
 from abc import ABC, abstractmethod
+import time
 
 
 class Dictionary(ABC):
@@ -33,28 +34,36 @@ class English(Dictionary):
     
   
     def search(self, word):
-        return requests.get(f'https://www.oxfordlearnersdictionaries.com/us/definition/english/{word}?q={word}', headers={'User-Agent': 'Mozilla/5.0'}).text
-        
+        return requests.get(f'https://dictionary.cambridge.org/us/dictionary/english/{word}', headers={'User-Agent': 'Mozilla/5.0'}).text
 
     def returnMeaning(self, word):
-        self.response = Selector(text=self.search(word))
         try:
-            self.text = self.response.xpath('//span[@class="def"]/text()').getall()
-            self.full_text = ''
-            self.howmany = 0
-            for r in range(len(self.text)):
-                if len(self.text[r].split())<3:
-                    self.howmany+=1
-                    ...
+            response = Selector(text=self.search(word))
+            text = response.xpath('//div[has-class("def", "ddef_d", "db")]//text()').getall()
+            concatenater, helper = [], ['']
+            full_text = ''
+            howmany = 0
+            for r in range (len(text)):
+                if text[r] == ': ':
+                    if helper[howmany] in concatenater:
+                        helper.pop()
+                        howmany-=1
+                    else:
+                        concatenater.append(helper[howmany])
+                    howmany+=1
+                    helper.append('')
                 else:
-                    self.full_text = self.full_text + f'{r+1-(self.howmany)}°: ' + self.text[r] + '\n\n'
+                    helper[howmany] = helper[howmany] + text[r]
+            for r in range(len(concatenater)):
+                full_text = full_text + f'{r+1}°: ' + concatenater[r] + '\n\n'
                 
-            if len(self.full_text)>0:
-                return self.full_text
+            if len(full_text)>0:
+                return True, full_text
             else:
-                return 'not found'
-        except:
-                return 'not found'
+                return False, 'not found'
+        except Exception as error:
+            print(error)
+            return False, error
 
 
 class Portuguese(Dictionary):
@@ -75,26 +84,32 @@ class Portuguese(Dictionary):
         import re
         word = unicodedata.normalize('NFD', word)
         word = re.sub('[\u0300-\u036f]', '', word)
-        
-        self.response = Selector(text=self.search(word))
+       
         try:
-            self.text = self.response.xpath('//p[@itemprop="description"]/span//text()').getall()
-            self.full_text = ''
-            self.howmany = 0
-            for r in range(1, len(self.text)-1):
-                if len(self.text[r].split())<3:
-                    self.howmany +=1
-                elif 'Etimologia' in self.text[r]:
+            response = Selector(text=self.search(word))
+            text = response.xpath('//p[@itemprop="description"]/span').getall()
+            full_text, helper = '', ''
+            howmany = 0
+            for r in range (1, len(text) -1):
+                text[r] = text[r].replace('<span>', '').replace('</span>', '').replace('<span class="tag">', '').replace('<i>', '').replace('</i>', '').replace('<a href="', '').replace('</a>', '')
+                if 'class="etim"' in text[r]:
                     break
+                elif 'class="cl"' in text[r]:
+                    howmany +=1
                 else:
-                    self.full_text = self.full_text + f'{r-self.howmany}°: ' + self.text[r] + '\n\n'
-                    
-            if len(self.full_text)>0:
-                return self.full_text
+                    while '>' in text[r]:
+                        where = text[r].find('>')
+                        helper = text[r][where+1:]
+                        where = text[r].find('/')
+                        text[r] = text[r][:where] + ' ' + helper
+                    full_text = full_text + f'{r-howmany}°: ' + text[r] + '\n\n'
+            if len(full_text)>0:
+                return True, full_text
             else:
-                return 'not found'
-        except:
-                return 'not found'
+                return False, 'not found'
+        except Exception as error:
+            print(error)
+            return False, error
         
 
 class Spanish(Dictionary):
@@ -115,25 +130,23 @@ class Spanish(Dictionary):
         import re
         word = unicodedata.normalize('NFD', word)
         word = re.sub('[\u0300-\u036f]', '', word)
-        
-        self.response = Selector(text=self.search(word))
         try:
-            self.text = self.response.xpath('//ol[@class="entry"]//li/text()').getall()
-            self.full_text = ''
-            self.howmany = 0
-            for r in range(len(self.text)):
-                if self.text[r] == ' ':
-                    self.howmany +=1
-                    ...
-                elif '♦' in self.text[r]:
-                    self.howmany +=1
-                    ...
-                else:
-                    self.full_text = self.full_text + f'{r+1-(self.howmany)}°: ' + self.text[r] + '\n'
-            if len(self.full_text)>0:
-                return self.full_text
+            response = Selector(text=self.search(word))
+            text = response.xpath('//ol[@class="entry"]//li').getall()
+            full_text, helper = '', ''
+            for r in range(len(text)):
+                text[r] = text[r].replace('<li>', '').replace('</li>', '').replace('<br>', '\n\t\t').replace('</span>', '').replace('<span>', '').replace('<i>', '').replace('</i>', '')
+                while '<span' in text[r]:
+                    where = text[r].find('<')
+                    helper = text[r][:where]
+                    where = text[r].find('>')
+                    text[r] = helper + ' ' + text[r][where+1:]
+                full_text = full_text + f'{r+1}°: ' + text[r] + '\n\n'
+            if len(full_text)>0:
+                return True, full_text
             else:
-                return 'Not found'
-        except:
-            return 'Not found'
+                return False, 'Not found'
+        except Exception as error:
+            print(error)
+            return False, error
 
