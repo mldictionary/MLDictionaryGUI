6ea608a22e14fcaa391023c38220df47be039982
utrape <pablosud025@gmail.com>
2021-04-26 22:25:55 -0300

    Update interface behavior

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ class Handler:
         self.play_sound = builder.get_object('play_sound')
         self.pronounce_spell = builder.get_object('pronounce_spell')
         self.is_to_play = False
+        self.pronounce_last_word = ''
 
     def onDestroy(self, *args):
         Gtk.main_quit()
@@ -37,9 +38,7 @@ class Handler:
         import re
         word = self.entry.get_text().lower().strip()
         word = re.sub('[^\w ]+', '', word)
-        if len(word)==0:
-            ...
-        else:
+        if len(word)>0:
             self.current_word.set_text(word.upper())
             if self.language.__repr__() == 'English':
                 self.current_source.set_markup(f'<a href="{self.language_source + f"dictionary/english/" + word}"> {self.language_source + f"dictionary/english/" + word}</a>')
@@ -52,9 +51,7 @@ class Handler:
                 self.label.set_justify(Gtk.Justification.LEFT)
                 self.label.set_selectable(True)
                 self.label.set_text(result[1])
-                
-                spell = self.pronounce.return_pronounce_spell(word, self.current_language.get_text())
-                self.pronounce_spell.set_text(spell)
+                self.pronounce_spell.set_text('Click on sound to see the word\'s pronounce spell')            
             else:
                 self.label.set_yalign(0.5)
                 self.label.set_xalign(0.5)
@@ -64,13 +61,20 @@ class Handler:
                 self.pronounce_spell.set_text('')
             self.is_to_play = result[0]
 
+
+    def show_up_pronounce_spell(self, word, language):
+        if word != self.pronounce_last_word:
+            self.pronounce_spell.set_text(self.pronounce.return_pronounce_spell(word, language))
+            self.pronounce_last_word = word
+
+ 
     def on_button_play_sound_clicked(self, widget):
         word = self.current_word.get_text().lower()
         if len(word.strip()) > 0 and self.is_to_play:
             self.pronounce.play_audio(word, self.language.__repr__())
-        
-
-   
+        self.show_up_pronounce_spell(word, self.current_language.get_text())
+                
+    
     def on_comboboxtext_changed(self, widget):
         if self.combo.get_active() == 0:
             self.language = webscrapy.English()
@@ -81,8 +85,7 @@ class Handler:
         elif self.combo.get_active() == 2:
             self.language = webscrapy.Spanish()
             self.language_source = 'https://www.wordreference.com/definicion/'
-
-        
+            
         self.current_language.set_text(self.language.__repr__() + ':')
         self.current_word.set_text('')
         self.current_source.set_text(self.language_source)
