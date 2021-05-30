a397200871ad8edba22a22a10cdc964f292437d8
utrape <pablosud025@gmail.com>
2021-05-30 16:09:10 -0300

    code improvement and bug fixes

--- webscrapy/pronunciation.py
+++ webscrapy/pronunciation.py
@@ -5,20 +5,20 @@ from playsound import playsound
 from shutil import copyfileobj, rmtree
 
 
-
 class Pronunciation:
     
     PATH = '.multilanguage_dictionary'
     PATH_AUDIO = PATH + '/audio'
     
-    def _search(self, word, language):
+    @staticmethod
+    def _search(word: str, language: str)->str:
         session = HTMLSession()
         r = session.get(f'https://www.google.com/search?q={word}+pronunciation+{language}&oq={word}+pronuncia+{language}')
         r.html.render()
         return r.text
     
     
-    def return_pronounce_spell(self, word, language):
+    def return_pronounce_spell(self, word: str, language: str)->str:
         element = Selector(text=self._search(word, language))
         html_element = element.css('span.seLqNc[jsname="dDjxrf"]::text').getall()
         if len(pronounce_spell := ' - '.join(html_element)) > 0:
@@ -27,7 +27,7 @@ class Pronunciation:
             return 'There is not pronounce spell available, put accent when is necessary'
         
     
-    def play_audio(self, word, language):
+    def play_audio(self, word: str, language: str)->None:
         word = word.replace(' ', '\ ')
         if language == 'English':
             play_word = word + '_en_us_1.mp3'
@@ -35,17 +35,18 @@ class Pronunciation:
             play_word = word + '_pt-BR_br_1.mp3'
         elif language == 'Spanish':
             play_word = word + '_es_es_1.mp3'
+        play_word_path = f'./{self.PATH_AUDIO}/{play_word}'
+        
+        if os.path.exists(self.PATH):
+            rmtree(self.PATH)
+        os.makedirs(self.PATH_AUDIO)
         try:
             url = f'https://ssl.gstatic.com/dictionary/static/pronunciation/2021-03-01/audio/{word[:2]}/{play_word}'
-            response = requests.get(url, stream=True)
-            if os.path.exists(self.PATH):
-                rmtree(self.PATH)
-            os.makedirs(self.PATH_AUDIO)
-            play_word_path = f'{self.PATH_AUDIO}/{play_word}'
-            with open(play_word_path, 'wb') as file:
+            with requests.get(url, stream=True) as response, \
+                open(play_word_path, 'wb') as file:
                 copyfileobj(response.raw, file)
             playsound(play_word_path)
-            rmtree(self.PATH)
         except Exception as error:
             print(error)
-        
+        rmtree(self.PATH)
+
