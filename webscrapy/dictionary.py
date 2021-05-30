893d7b088b83e4b30d624521098410685cbe3cda
utrape <pablosud025@gmail.com>
2021-05-30 02:23:12 -0300

    code review, typing and improvement

--- webscrapy/dictionary.py
+++ webscrapy/dictionary.py
@@ -1,16 +1,15 @@
-import requests, re
+import requests, re, unicodedata
 from parsel import Selector
 from abc import ABC, abstractmethod
-from typing import List
-# from multiprocessing import Process
+from typing import List, Union
 
 class Dictionary(ABC):
     URL: str
     XPATH: str
     
     @abstractmethod
-    def __repr__(self):
-        ...
+    def __repr__(self)->str:
+        return 'Dictionary'
 
     
     @classmethod
@@ -24,7 +23,7 @@ class Dictionary(ABC):
 
 
     @abstractmethod
-    def return_meaning(self, word):
+    def return_meaning(self, word: str)->Union[str, bool]:
         ...
 
 
@@ -32,14 +31,14 @@ class English(Dictionary):
     URL = 'https://dictionary.cambridge.org/us/dictionary/english/{}'
     XPATH = '//div[has-class("def", "ddef_d", "db")]'
     
-    def __repr__(self):
+    def __repr__(self)->str:
         return 'English'
 
 
-    def return_meaning(self, word):
+    def return_meaning(self, word: str)->Union[str, bool]:
         try:
             if len(text := self._get_meanings(word))>0:
-                text = list(map(lambda arr: re.sub('<[^>]*>', '', arr), text))
+                text = list(map(lambda mean: re.sub('<[^>]*>', '', mean), text))
                 formatted_text = ''
                 how_many = 0
                 for i in range(len(text)):
@@ -48,78 +47,64 @@ class English(Dictionary):
                         continue
                     else:
                         formatted_text += f'{i+1-how_many}º: ' + text[i].replace(':', '.') + '\n\n'
-                if len(formatted_text)>0:
-                    return True, formatted_text.replace('\n        \n         ', ':  ')
-                else:
-                    return False, 'not found'
+                return formatted_text.replace('\n        \n         ', ':  ') or False
             else:
-                return False, 'not found'
+                return False
         except Exception as error:
             print(error)
-            return False, error
+            return False
 
 
 class Portuguese(Dictionary):
     URL = 'https://www.dicio.com.br/{}/'
     XPATH = '//p[@itemprop="description"]/span'
     
-    def __repr__(self):
+    def __repr__(self)->str:
         return 'Portuguese'
 
 
-    def return_meaning(self, word):
-        import unicodedata
+    def return_meaning(self, word: str)->Union[str, bool]:
         word = unicodedata.normalize('NFD', word)
         word = re.sub('[\u0300-\u036f]', '', word)
-    
         try:
             if len(text := self._get_meanings(word))>0:
-                formatted_text = ''
                 which_one = 0
-                def text_formatter(arr)->str:
+                def text_formatter(mean: str)->str:
                     nonlocal which_one
-                    if 'class="cl"' in arr or 'class="etim"' in arr:
+                    if 'class="cl"' in mean or 'class="etim"' in mean:
                         return ''
                     which_one+=1
-                    return f'{which_one}º: {re.sub("<[^>]*>", "", arr)}\n\n'
-                if len(formatted_text := ''.join(list(map(text_formatter, text))))>0:
-                    return True, formatted_text
-                else:
-                    return False, 'not found'
+                    return f'{which_one}º: {re.sub("<[^>]*>", "", mean)}\n\n'
+                return ''.join(list(map(text_formatter, text))) or False
             else:
-                return False, 'not found'
+                return False
         except Exception as error:
             print(error)
-            return False, error
+            return False
         
 
 class Spanish(Dictionary):
     URL = 'https://www.wordreference.com/definicion/{}'
     XPATH = '//ol[@class="entry"]//li'
     
-    def __repr__(self):
+    def __repr__(self)->str:
         return 'Spanish'
     
     
-    def return_meaning(self, word):
-        import unicodedata
+    def return_meaning(self, word: str)->Union[str, bool]:
         word = unicodedata.normalize('NFD', word)
         word = re.sub('[\u0300-\u036f]', '', word)
         try:
             if len(text := self._get_meanings(word))>0:
                 which_one = 0
-                def text_formatter(arr)->str:
+                def text_formatter(mean: str)->str:
                     nonlocal which_one
                     which_one+=1
-                    arr = arr.replace('<br>', '\n\t\t')
-                    return f'{which_one}º:' + re.sub("<[^>]*>", "", arr)
-                if len(formatted_text := '\n\n'.join(list(map(text_formatter, text))))>0:
-                    return True, formatted_text
-                else:
-                    return False, 'Not found'
+                    mean = mean.replace('<br>', '\n\t\t')
+                    return f'{which_one}º:' + re.sub("<[^>]*>", "", mean)
+                return '\n\n'.join(list(map(text_formatter, text))) or False
             else:
-                return False, 'not found'
+                return False
         except Exception as error:
             print(error)
-            return False, error
-
+            return False
